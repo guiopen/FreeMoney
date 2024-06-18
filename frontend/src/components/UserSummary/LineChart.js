21f71d7e61bb2722bebb7eb5f1c25964fb669320
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-06-18 10:25:59 -0300

    componente grafico de linha

--- frontend/src/components/UserSummary/LineChart.js
+++ frontend/src/components/UserSummary/LineChart.js
@@ -1,12 +1,61 @@
 import { useState } from "react"
 import Radio from "./Radio"
+import { Line } from 'react-chartjs-2';
+import fakeTransactions from "../../data/fakeTransactions";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement
+} from 'chart.js';
 
 export default function LineChart() {
   const [selected, setSelected] = useState("expenses")
 
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
+
   function handleSelect(ev) {
     setSelected(ev.target.value)
   }
+
+  const expensesByMonth = fakeTransactions
+    .reduce((obj, transaction) => {
+      const month = transaction.date.getMonth()
+      if (transaction.expense) {
+        obj[month] += transaction.value
+      }
+      return obj
+    }, { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 })
+
+  const incomesByMonth = fakeTransactions
+    .reduce((obj, transaction) => {
+      const month = transaction.date.getMonth()
+      if (!transaction.expense) {
+        obj[month] += transaction.value
+      }
+      return obj
+    }, { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 })
+
+  const expensesData = {
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    datasets: [
+      {
+        data: Object.values(expensesByMonth),
+        borderColor: "red",
+      }
+    ]
+  };
+
+  const incomesData = {
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    datasets: [
+      {
+        data: Object.values(incomesByMonth),
+        borderColor: "green",
+      }
+    ]
+  };
 
   return (
     <div className="w-full">
@@ -30,6 +79,15 @@ export default function LineChart() {
             name="line"
           />
         </span>
+      </div>
+      <div className="w-full">
+        <Line
+          className="mx-auto mt-3 max-w-64 sm:max-w-96 md:max-w-[600px]"
+          id="0"
+          data={selected === "expenses" ? expensesData : incomesData}
+          options={{ responsive: false, devicePixelRatio: 10 }}
+          style={{ width: "100%" }}
+        />
       </div>
     </div>
   )
--- frontend/src/pages/UserSummary/Summary.js
+++ frontend/src/pages/UserSummary/Summary.js
@@ -4,15 +4,15 @@ import TransactionHistories from "../../components/UserSummary/TransactionHistor
 
 export default function Summary() {
   return (
-    <div className="w-screen min-h-[calc(100vh-96px)] py-24 px-4 lg:px-8
-      bg-[#D8FDFF] 
+    <div className="w-full min-h-[calc(100vh-96px)] py-12 px-4 lg:px-8
       flex flex-col lg:flex-row justify-center gap-8
+      bg-[#D8FDFF] 
     ">
-      <div className="flex flex-col gap-8 lg:flex-1 w-full">
-        <div className="w-full p-4 rounded-md bg-white max-lg:flex-1 h-[50%]">
+      <div className="flex flex-col gap-8 lg:flex-1 w-full h-full">
+        <div className="w-full p-4 rounded-md bg-white lg:flex-1">
           <LineChart />
         </div>
-        <div className="w-full p-4 rounded-md bg-white h-[50%]">
+        <div className="w-full p-4 rounded-md bg-white">
           <PizzaChart />
         </div>
       </div>
