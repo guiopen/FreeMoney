d38ed9e7277cb910ef15f38ee39d217834556532
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-06-18 10:56:28 -0300

    adiciona mais falsos dados

--- frontend/src/data/fakeTransactions.js
+++ frontend/src/data/fakeTransactions.js
@@ -10,13 +10,29 @@ const fakeTransactions = [
   {
     id: 1,
     title: "Conta de água",
-    date: new Date(),
+    date: new Date(new Date().setMonth(3)),
     category: "Casa",
     value: 489.2,
     expense: true
   },
   {
     id: 2,
+    title: "Mensalidade cursos",
+    date: new Date(new Date().setMonth(1)),
+    category: "Profissão",
+    value: 389.2,
+    expense: true
+  },
+  {
+    id: 3,
+    title: "Gás",
+    date: new Date(new Date().setMonth(6)),
+    category: "Casa",
+    value: 189.2,
+    expense: true
+  },
+  {
+    id: 4,
     title: "Estágio",
     date: new Date(),
     category: "Salário",
@@ -24,7 +40,23 @@ const fakeTransactions = [
     expense: false,
   },
   {
-    id: 3,
+    id: 5,
+    title: "Freelance",
+    date: new Date(new Date().setMonth(2)),
+    category: "Serviço",
+    value: 2789.2,
+    expense: false,
+  },
+  {
+    id: 6,
+    title: "Trampo",
+    date: new Date(new Date().setMonth(6)),
+    category: "Serviço",
+    value: 1789.2,
+    expense: false,
+  },
+  {
+    id: 7,
     title: "Dividendos",
     date: new Date(),
     category: "Investimentos",
