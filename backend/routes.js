d99792386d2ad8fc7122e41a84eb60451766aa4b
marinabenvenuti <marinabcardeal@gmail.com>
2024-06-20 22:10:15 -0300

    base da sessão

--- backend/routes.js
+++ backend/routes.js
@@ -1,47 +1,17 @@
+require("dotenv").config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const { generateRandomNumber } = require('./utils/generateRandomCode');
 const { encryptPassword, verifyPassword } = require('./utils/passwordEncryption');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-// Configuração da URL de conexão do MongoDB
-const mongoURL = "mongodb+srv://free:money@freemoneycluster.fzca9rk.mongodb.net/?retryWrites=true&w=majority&appName=FreeMoneyCluster";
-const dbName = "freeMoneyDb";
-let database;
+// Simulando um banco de dados em memória
+const users = [];
 
-MongoClient.connect(mongoURL, { useUnifiedTopology: true })
-  .then(client => {
-    console.log("Conectado ao MongoDB");
-    database = client.db(dbName);
-  })
-  .catch(error => console.error(error));
-
-router.get('/nomes', (req, res) => {
-  const nomes = ["Guilherme", "Olavo", "Marina", "Alexandre", "Jilliard"];
-  res.json(nomes);
-});
-
-const fakeTransactions = [
-  {
-    id: 0,
-    title: "Conta de Luz",
-    date: new Date(),
-    category: "Casa",
-    value: 789.2,
-    expense: true,
-  },
-  {
-    id: 4,
-    title: "Estágio",
-    date: new Date(),
-    category: "Salário",
-    value: 1789.2,
-    expense: false,
-  }
-]
 // Rota para registrar um novo usuário
-router.post('/register_user', async (req, res) => {
-  const { name, email, password } = req.body;
+router.post('/register', (req, res) => {
+  const { id, email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({ message: 'Email e senha são obrigatórios' });
@@ -73,36 +43,90 @@ router.post('/register_user', async (req, res) => {
     console.error(error);
     res.status(500).json({ message: 'Erro ao registrar usuário' });
   }
+
+  const newUser = { id, email, password };
+  users.push(newUser);
+
+  res.status(201).json({ message: 'Usuário cadastrado com sucesso' });
 });
 
-// Rota para pegar o histórico de outro usuario
-router.get('/friend_history', async (req, res) => {
-  const { email, code } = req.query; // Obtém o email e o código da query string
+// Rota para realizar o login e gerar o token JWT
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-  if (!email || !code) {
-    return res.status(400).json({ message: 'Email e código são obrigatórios' });
+  // validações
+  if (!email || !password) {
+    return res.status(422).json({ msg: "Email e senha são obrigatórios!" });
+  }
+
+  // checando se o usuário existe no "banco de dados" em memória
+  const user = users.find(user => user.email === email);
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  // checando se a senha corresponde
+  if (user.password !== password) {
+    return res.status(422).json({ msg: "Senha inválida!" });
   }
 
   try {
-    const usersCollection = database.collection('users');
+    const secret = process.env.SECRET;
 
-    const user = await usersCollection.findOne({ email: email });
-    if (!user) {
-      return res.status(404).json({ message: 'Usuário não encontrado' });
-    }
-    if (user.code !== code) {
-      return res.status(401).json({ message: 'Código inválido' });
+    if (!secret) {
+      console.log("Chave secreta não configurada");
     }
 
-    res.status(200).json({ history: user.history }); 
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email 
+      },
+      secret
+    );
+
+    res.status(200).json({ message: "Autenticação realizada com sucesso!", token });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Erro ao buscar histórico de transações' });
+    console.error(error); 
+    res.status(500).json({ message: "Erro ao autenticar usuário!" });
   }
 });
 
+// verificar o token JWT
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Acesso negado! Token não fornecido." });
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Token inválido!" });
+  }
+}
+
+// Rota privada para obter informações do usuário por ID
+router.get("/user/:id", checkToken, (req, res) => {
+  const id = req.params.id;
+
+  // Checando se o usuário existe 
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  // Não enviamos a senha do usuário na resposta
+  const userWithoutPassword = { ...user };
+  delete userWithoutPassword.password;
+
+  res.status(200).json({ user: userWithoutPassword });
+});
 
 module.exports = router;
-
-
-
