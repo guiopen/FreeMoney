6000d0d9ef8feab872fa29a87a556cb5db2b9652
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-06-24 08:22:28 -0300

    edita estilos summary

--- frontend/src/pages/UserSummary/Summary.js
+++ frontend/src/pages/UserSummary/Summary.js
@@ -22,26 +22,28 @@ export default function Summary() {
   // fetchTransactions()
   // }, [transactions])
   return (
-    <div className="w-full min-h-[calc(100vh-96px)] py-12 px-4 lg:px-8
-      flex flex-col lg:flex-row justify-center gap-8
-      bg-[#D8FDFF] 
-    ">
-      <div className="flex flex-col gap-8 lg:flex-1 w-full h-full">
-        <div className="w-full p-4 rounded-md bg-white lg:flex-1">
-          <LineChart
+    <div className="bg-[#D8FDFF] w-full h-full min-h-[calc(100vh-96px)]">
+      <div className="w-full py-12 px-4 lg:px-8
+        flex flex-col lg:flex-row justify-center gap-8
+        max-w-[1024px] mx-auto
+      ">
+        <div className="flex flex-col gap-8 lg:flex-1 w-full h-full">
+          <div className="w-full p-4 rounded-md bg-white lg:flex-1">
+            <LineChart
+              transactions={fakeTransactions}
+            />
+          </div>
+          <div className="w-full p-4 rounded-md bg-white">
+            <PizzaChart
+              transactions={fakeTransactions}
+            />
+          </div>
+        </div>
+        <div className="p-4 rounded-md bg-white lg:flex-1 w-full">
+          <TransactionHistories
             transactions={fakeTransactions}
           />
         </div>
-        <div className="w-full p-4 rounded-md bg-white">
-          <PizzaChart
-            transactions={fakeTransactions}
-          />
-        </div>
-      </div>
-      <div className="p-4 rounded-md bg-white lg:flex-1 w-full">
-        <TransactionHistories 
-          transactions={fakeTransactions}
-        />
       </div>
     </div>
   )
