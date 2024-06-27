c5b6a08be2bfe020e6ec2801eef296326f239a0a
marinabenvenuti <marinabcardeal@gmail.com>
2024-06-27 14:45:58 -0300

    sessão de login apenas no backend

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,13 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const { generateRandomNumber } = require('./utils/generateRandomCode');
 const { encryptPassword, verifyPassword } = require('./utils/passwordEncryption');
+const { checkToken } = require('./utils/checkToken');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+
+// models
+const User = require("./models/User");
 
 // Configuração da URL de conexão do MongoDB
 const mongoURL = "mongodb+srv://free:money@freemoneycluster.fzca9rk.mongodb.net/?retryWrites=true&w=majority&appName=FreeMoneyCluster";
@@ -16,6 +22,7 @@ MongoClient.connect(mongoURL, { useUnifiedTopology: true })
     database = client.db(dbName);
   })
   .catch(error => console.error(error));
+
 
 router.get('/nomes', (req, res) => {
   const nomes = ["Guilherme", "Olavo", "Marina", "Alexandre", "Jilliard"];
@@ -76,6 +83,75 @@ router.post('/register_user', async (req, res) => {
   }
 });
 
+// Rota para realizar o login e gerar o token JWT
+router.post("/login_user", async (req, res) => {
+  const usersCollection = database.collection('users');
+  const { email, password } = req.body;
+
+  // validações
+  if (!email || !password) {
+    return res.status(422).json({ msg: "Email e senha são obrigatórios!" });
+  }
+
+  // checando se o usuário existe
+  const user = await usersCollection.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  // checando se a senha bate
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ msg: error });
+  }
+});
+
+// Rota privada de teste
+router.get("/user/:id", checkToken, async (req, res) => {
+  const usersCollection = database.collection('users');
+  const id = req.params.id;
+
+  try {
+    // Verifica se o ID é válido
+    const ObjectId = require('mongodb').ObjectId;
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ msg: "ID inválido!" });
+    }
+
+    // Busca o usuário pelo ID
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) }, { projection: { password: 0 } });
+
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado!" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Erro ao buscar usuário" });
+  }
+});
+
+
+
 // Rota para pegar o histórico de outro usuario
 router.get('/friend_history', async (req, res) => {
   const { email, code } = req.query; // Obtém o email e o código da query string
@@ -104,6 +180,3 @@ router.get('/friend_history', async (req, res) => {
 
 
 module.exports = router;
-
-
-
